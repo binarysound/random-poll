f2fe4e508181c5dea7ac627b463b1d6c60eae518
Jeongho Jeong <jeongho@styleshare.kr>
2017-06-11 18:13:04 +0900

    [#1] 기초 프로젝트 셋업 진행
    - SQLAlchemy 적용
    - Config 지정 가능하도록 함

--- README.md
+++ README.md
@@ -1,5 +1,16 @@
-## Install & Run
+#### Installing Dependencies
 ```bash
 pip install -r requirements.txt
+```
+
+#### Initializing Database
+First, you need to install PSQL. Then you need to create proper database. When you're ready, proceed to create tables. Open up your python shell and continue.
+```python
+from main import db
+db.create_all()
+```
+
+#### Running Server
+```bash
 python main.py
 ```
--- main.py
+++ main.py
@@ -1,9 +1,55 @@
+import configparser
+import random
+import string
+from optparse import OptionParser
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+parser = OptionParser()
+parser.add_option("-c", "--config", dest="config", help="configuration file")
+
+(options, _) = parser.parse_args()
+
+config_file = options.config if options.config else "dev.cfg"
+
+config = configparser.ConfigParser()
+config.read(config_file)
+
+db_config = config["database"]
+
 app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = (
+    "postgresql://%s:%s@%s/%s" % (
+        db_config["user"],
+        db_config["password"],
+        db_config["host"],
+        db_config["database"]
+    )
+)
+db = SQLAlchemy(app)
+
+app_config = config["app"]
+port = 7654
+
+if app_config:
+    if app_config["port"]:
+        port = app_config["port"]
+
+
+class Question(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.String(140))
+
+    def __init__(self, content):
+        self.content = content
+
+    def __repr__(self):
+        return "<Question %r>" % self.content
+
 
 @app.route("/")
 def hello():
     return "Hello world!"
 
 if __name__ == "__main__":
-    app.run(port=7654)
+    app.run(port=port)
