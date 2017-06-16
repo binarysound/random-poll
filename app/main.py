eff21ab9d77e6863a153979940c21093f25ed7f4
HS Lim <bookman1209@gmail.com>
2017-06-16 14:25:07 +0900

    [#8] 프로젝트 구조 재구성
     - manage.py 작성
     - Dockerfile 변경
     - main.py의 parsing logic을 manage.py로 이전

--- app/main.py
+++ app/main.py
@@ -5,36 +5,15 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
 parser = OptionParser()
-parser.add_option("-e", "--env", dest="env", help="environment configuration file")
+parser.add_option("-p", "--db-password", dest="db_password", help="password of database")
 
 (options, _) = parser.parse_args()
-
-env_file = options.env if options.env else ".env"
-
-class EnvParser(object):
-    def __init__(self):
-        pass
-
-    def parse(self, env_file):
-        env = {}
-        with open(env_file, "r") as f:
-            for line in f:
-                if line[0] == "#": pass  # This is comment line
-                parsed = line.strip().split("=")
-                if len(parsed) != 2:
-                    print("Ill-formed .env file")
-                    exit(1)
-                env[parsed[0]] = parsed[1]
-        return env
-
-env_parser = EnvParser()
-env = env_parser.parse(env_file)
 
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = (
     "postgresql://%s:%s@%s/%s" % (
         "postgres",
-        env["DB_PASSWORD"],
+        options.db_password,
         "postgres",
         "postgres"
     )
